6438d8e08485d5b88da8a633c3561fa1dfff56d8
Rohan Daddekar <rohandaddekar9@gmail.com>
2025-01-15 17:06:07 +0530

    deleted Tab folder from base-comp/Headless

--- src/base-components/Headless/index.ts
+++ src/base-components/Headless/index.ts
@@ -1,8 +1,7 @@
 import Menu from "./Menu";
 import Popover from "./Popover";
-import Tab from "./Tab";
 import Dialog from "./Dialog";
 import Disclosure from "./Disclosure";
 import Slideover from "./Slideover";
 
-export { Menu, Popover, Tab, Dialog, Disclosure, Slideover };
+export { Menu, Popover, Dialog, Disclosure, Slideover };
